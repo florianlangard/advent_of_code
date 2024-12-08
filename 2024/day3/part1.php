f56155cf8ccd549db0b2347e8370686b47915e17
florianlangard <florian.langard@gmail.com>
2024-12-08 11:53:15 +0100

    updated day 3

--- 2024/day3/part1.php
+++ 2024/day3/part1.php
@@ -3,20 +3,31 @@
 memory_reset_peak_usage();
 $start_time = microtime(true);
 
-$raw = file_get_contents('./sample.txt');
+$data = file_get_contents('./data.txt');
 // $data = str_replace("\n\n"," ",$raw);
-$data = explode("\n", $raw);
+// $data = explode("\n", $raw);
 
 $result = "";
 
 //= Part One =========================================
+preg_match_all('/mul\(\d+,\d+\)/', $data, $matches);
 
+$pr = [];
+if ($matches[0]) {
 
+    foreach ($matches[0] as $key => $item) {
+
+        preg_match('/\d+,\d+/', $item, $mul);
+        $r = explode(",", $mul[0]);
+        $pr[] = array_product($r);
+
+    }
+}
 
 
 echo "<pre>";
-print_r($result);
+print_r(array_sum($pr));
 echo "</pre>";
 
 echo "<pre>Execution time: ".round(microtime(true) - $start_time, 4)." seconds\n";
-echo "   Peak memory: ".round(memory_get_peak_usage()/pow(2, 20), 4), " MiB\n\n </pre>";
+echo "Peak memory: ".round(memory_get_peak_usage()/pow(2, 20), 4), " MiB\n\n </pre>";
--- 2024/day3/part2.php
+++ 2024/day3/part2.php
@@ -3,20 +3,39 @@
 memory_reset_peak_usage();
 $start_time = microtime(true);
 
-$raw = file_get_contents('./sample.txt');
+$data = file_get_contents('./data.txt');
 // $data = str_replace("\n\n"," ",$raw);
-$data = explode("\n", $raw);
+// $data = explode("\n", $raw);
 
 $result = "";
 
 //= Part Two =========================================
+unset($matches, $r);
+$pr = [];
 
+preg_match_all('/mul\(\d+,\d+\)|do\(\)|don\'t\(\)/', $data, $matches);
 
+$isActive = true;
+
+foreach ($matches[0] as $key => $item) {
+
+    if ($item == "don't()") {
+        $isActive = false;
+    }
+    elseif ($item == "do()") {
+        $isActive = true;
+    }
+    elseif ($isActive) {
+        preg_match('/\d+,\d+/', $item, $mul);
+        $r = explode(",", $mul[0]);
+        $pr[] = array_product($r);
+    }
+}
 
 
 echo "<pre>";
-print_r($result);
+print_r(array_sum($pr));
 echo "</pre>";
 
 echo "<pre>Execution time: ".round(microtime(true) - $start_time, 4)." seconds\n";
-echo "   Peak memory: ".round(memory_get_peak_usage()/pow(2, 20), 4), " MiB\n\n </pre>";
+echo "Peak memory: ".round(memory_get_peak_usage()/pow(2, 20), 4), " MiB\n\n </pre>";
